de04777f9b4b2a6057ecd7ccb58885765bf3ca61
JubayerDev <ajashes858@gmail.com>
2022-12-21 23:19:27 +0600

    hero section and navbar added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../Bannar/Banner';
+
+const Home = () => {
+    return (
+        <div className='mx-5'>
+            <Banner />
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: '#0FCFEC',
+          secondary: '#19D3AE',
+          accent: '#3A4256',
+          "base-100": '#FFFFFF'
+        }
+      }
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
